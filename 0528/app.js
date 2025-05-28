c6691b99137296c620de6eeade514d2d2c5e5fd1
boonmojae <pminny99@naver.com>
2025-05-28 17:22:44 +0900

    app.js

--- 0528/app.js
+++ 0528/app.js
@@ -7,11 +7,14 @@ app.set("view engine", "ejs");
 app.use("/views", express.static(__dirname + "/views"));
 app.use("/public", express.static(__dirname + "/public"));
 
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
 app.get("/", function (req, res) {
   res.render("index", { title: "폼 실습을 해봅시다" });
 });
 
-app.get("/getForm", function (req, res) {
+app.get("/getForm", (req, res) => {
   console.log(req.query);
 
   res.render("result", {
@@ -19,6 +22,15 @@ app.get("/getForm", function (req, res) {
     userInfo: req.query,
   });
 });
+
+app.post("/postForm", (req, res) => {
+  console.log(req.query);
+
+  res.render("result", {
+    title: "Post 요청 폼 결과 확인하기", 
+    userInfo: req.body
+  });
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
